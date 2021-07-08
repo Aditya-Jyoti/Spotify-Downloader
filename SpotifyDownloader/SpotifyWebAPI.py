933a1e2fb1fe11f011561697f01609c9dc837bf5
Hugo Moracchini <hugo4400@gmail.com>
2021-07-09 00:16:32 +0200

    Addded pagination in spotify API and linted code

--- SpotifyDownloader/SpotifyWebAPI.py
+++ SpotifyDownloader/SpotifyWebAPI.py
@@ -2,10 +2,6 @@ import requests
 from urllib.parse import urlencode
 from urllib.request import urlopen
 from urllib.error import HTTPError
-import requests
-from urllib.parse import urlencode
-from urllib.request import urlopen
-from urllib.error import HTTPError
 import re
 import json
 from base64 import b64encode
@@ -13,44 +9,48 @@ from base64 import b64encode
 
 def get_playlists(spotify_url):
     with open('MY_SECRETS.json', 'r') as f:
-        load_file = json.load(f)
-        spotify_key = load_file['SPOTIFY_KEY']
-    
-    headers = {
-        'Authorization': f'Bearer {spotify_key}'
-    }
+        spotify_key = json.load(f)['SPOTIFY_KEY']
 
     playlist_id = spotify_url.split('/')[-1].split('?')[0]
-    r = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}", headers=headers)
+    r = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}", headers={'Authorization': f'Bearer {spotify_key}'})
 
-    if str(r) == '<Response [400]>' or str(r) == '<Response [401]>':
+    if r.status_code == 400 or r.status_code == 401:
         raise TypeError('Invalid Spotify Token')
 
-    else:
-        returned_tracks = {}
+    returned_tracks = {}
 
-        playlist_name = r.json()['name']
+    playlist_name = r.json()['name']
 
-        for track in r.json()['tracks']['items']:
-            song_name = track['track']['name']
-            artists = []
+    r = requests.get(f"https://api.spotify.com/v1/playlists/{playlist_id}/tracks", headers={'Authorization': f'Bearer {spotify_key}'})
+    data = r.json()
 
-            for artist in track['track']['artists']:
-                artists.append(artist['name'])
-            artist_name = ' '.join(artists)
+    tracks = data['items']
+    while data['next']:
+        r = requests.get(data['next'], headers={'Authorization': f'Bearer {spotify_key}'})
+        data = r.json()
+        tracks = tracks + data['items']
 
-            try:
-                query_string = urlencode({'search_query': artist_name + ' ' + track['track']['name']})
-                htm_content = urlopen('http://www.youtube.com/results?' + query_string)
-                search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
+    for track in tracks:
+        song_name = track['track']['name']
+        artists = []
 
-                returned_tracks.update({f'{song_name}': f'http://www.youtube.com/watch?v={search_results[0]}'})
+        for artist in track['track']['artists']:
+            artists.append(artist['name'])
+        artist_name = ' '.join(artists)
 
-            except HTTPError:
-                print(f'Couldn\'t download "{song_name}", continuing')
-                continue
+        try:
+            query_string = urlencode({'search_query': artist_name + ' ' + song_name})
+            htm_content = urlopen('http://www.youtube.com/results?' + query_string)
+            search_results = re.findall(r'/watch\?v=(.{11})', htm_content.read().decode())
 
-        return playlist_name, returned_tracks
+            returned_tracks.update({f'{song_name}': f'http://www.youtube.com/watch?v={search_results[0]}'})
+
+        except HTTPError:
+            print(f'Couldn\'t download "{song_name}", continuing')
+            continue
+
+    return playlist_name, returned_tracks
+
 
 def get_access_token():
     with open('MY_SECRETS.json', 'r') as f:
@@ -58,7 +58,6 @@ def get_access_token():
         spotify_client_id = load_file['spotify_client_id']
         spotify_client_secret = load_file['spotify_client_secret']
 
-    
     headers = {
         'Authorization': f'Basic {b64encode(f"{spotify_client_id}:{spotify_client_secret}".encode()).decode()}',
     }
@@ -67,7 +66,7 @@ def get_access_token():
         'grant_type': 'client_credentials'
     }
 
-    r= requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
+    r = requests.post('https://accounts.spotify.com/api/token', headers=headers, data=data)
 
     token = r.json()['access_token']
 
